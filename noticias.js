c9cd7fce0c25256368e0972cf2241f53a2013cb1
theyounboss06 <theyoungboss00@gmail.com>
2025-04-13 01:16:15 -0300

    se actualiso todos el html

--- noticias.js
+++ noticias.js
@@ -1,25 +1,25 @@
-fetch('https://deultimominuto.net/wp-json/wp/v2/posts')
-.then(res => res.json())
-.then(data => {
-  const container = document.getElementById('noticias');
+  fetch('https://deultimominuto.net/wp-json/wp/v2/posts?per_page=20')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('noticias');
 
-  data.forEach(noticia => {
-    const card = document.createElement('div');
-    card.className = 'noticia-card';
+    data.forEach(noticia => {
+      const card = document.createElement('div');
+      card.className = 'noticia-card';
 
-    const titulo = noticia.title.rendered;
-    const imagen = noticia.jetpack_featured_media_url;
-    const id = noticia.id;
+      const titulo = noticia.title.rendered;
+      const imagen = noticia.jetpack_featured_media_url;
+      const id = noticia.id;
 
-    card.innerHTML = `
-      <img src="${imagen}" alt="Imagen de la noticia">
-      <h3>${titulo}</h3>
-      <a href="noticia.html?id=${id}">
-        <button class="btn-leer">Leer más</button>
-      </a>
-    `;
+      card.innerHTML = `
+        <img src="${imagen}" alt="Imagen de la noticia">
+        <h3>${titulo}</h3>
+        <a href="noticia.html?id=${id}">
+          <button class="btn-leer">Leer más</button>
+        </a>
+      `;
 
-    container.appendChild(card);
-  });
-})
-.catch(err => console.error('Error al cargar las noticias:', err));
+      container.appendChild(card);
+    });
+  })
+  .catch(err => console.error('Error al cargar las noticias:', err));
